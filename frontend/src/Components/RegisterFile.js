3354ab4a9224910768b32d1b4bf423b0646346c7
Surya_M_S_7 <sur.ms2001@gmail.com>
2022-06-01 15:21:26 +0530

    4) Casual commit..

--- frontend/src/Components/RegisterFile.js
+++ frontend/src/Components/RegisterFile.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import Web3_MS from 'web3/dist/web3.min.js'
-// import SupplyChainABI from "./artifacts/SupplyChain.json"
 import { useNavigate } from "react-router-dom"
+// import { address, abi } from "./constants/index";
+import ABI from "../artifacts/SupplyChain.json"
 
 const Register_Comp = () => {
+    const [currentaccount, setCurrentaccount] = useState("");
+    const [loader, setloader] = useState(true);
+    const [SupplyChain, setSupplyChain] = useState();
+    const [RMSname, setRMSname] = useState();
+    const [MANname, setMANname] = useState();
+    const [DISname, setDISname] = useState();
+    const [RETname, setRETname] = useState();
+    const [RMSplace, setRMSplace] = useState();
+    const [MANplace, setMANplace] = useState();
+    const [DISplace, setDISplace] = useState();
+    const [RETplace, setRETplace] = useState();
+    const [RMSaddress, setRMSaddress] = useState();
+    const [MANaddress, setMANaddress] = useState();
+    const [DISaddress, setDISaddress] = useState();
+    const [RETaddress, setRETaddress] = useState();
+    const [RMS, setRMS] = useState();
+    const [MAN, setMAN] = useState();
+    const [DIS, setDIS] = useState();
+    const [RET, setRET] = useState();
+
     const nav = useNavigate()
     useEffect(() => {
         loadWeb3();
-        // loadBlockchaindata();
+        loadBlockchaindata();
     }, [])
 
     //Below function invokes metamask
@@ -23,6 +44,55 @@ const Register_Comp = () => {
             );
         }
     };
+
+    const loadBlockchaindata = async () => {
+        setloader(true);
+        const web3 = window.web3;
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        // console.log(account);
+        setCurrentaccount(account);
+        const networkId = await web3.eth.net.getId();
+        // console.log(networkId);
+        const networkData = ABI.networks[networkId];
+        // console.log(networkData);
+        // console.log("Hello", networkData.address);
+        if (networkData) {
+            console.log("Im true");
+            const supplychain = new web3.eth.Contract(ABI.abi, networkData.address);
+            console.log("Im true");
+        //     setSupplyChain(supplychain);
+        //     var i;
+        //     const rmsCtr = await supplychain.methods.rmsCtr().call();
+        //     const rms = {};
+        //     for (i = 0; i < rmsCtr; i++) {
+        //         rms[i] = await supplychain.methods.RMS(i + 1).call();
+        //     }
+        //     setRMS(rms);
+        //     const manCtr = await supplychain.methods.manCtr().call();
+        //     const man = {};
+        //     for (i = 0; i < manCtr; i++) {
+        //         man[i] = await supplychain.methods.MAN(i + 1).call();
+        //     }
+        //     setMAN(man);
+        //     const disCtr = await supplychain.methods.disCtr().call();
+        //     const dis = {};
+        //     for (i = 0; i < disCtr; i++) {
+        //         dis[i] = await supplychain.methods.DIS(i + 1).call();
+        //     }
+        //     setDIS(dis);
+        //     const retCtr = await supplychain.methods.retCtr().call();
+        //     const ret = {};
+        //     for (i = 0; i < retCtr; i++) {
+        //         ret[i] = await supplychain.methods.RET(i + 1).call();
+        //     }
+        //     setRET(ret);
+        //     setloader(false);
+        }
+        // else {
+        //     window.alert('The smart contract is not deployed to current network')
+        // }
+    }
 
     return (
         <div>
---
+++ frontend/src/Components/constants/index.js
@@ -0,0 +1,470 @@
+export const address = '0xdfe62bd43e140616893E174797D1F63B49f46A20';
+export const abi =  [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "DIS",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "place",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "Distribute",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "MAN",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "place",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "Manufacturing",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "MedicineStock",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "RMSid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "MANid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "DISid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "RETid",
+          "type": "uint256"
+        },
+        {
+          "internalType": "enum SupplyChain_Contract.STAGE",
+          "name": "stage",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "Owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "RET",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "place",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "RMS",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "place",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "RMSsupply",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "Retail",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_place",
+          "type": "string"
+        }
+      ],
+      "name": "addDistributor",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_place",
+          "type": "string"
+        }
+      ],
+      "name": "addManufacturer",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_description",
+          "type": "string"
+        }
+      ],
+      "name": "addMedicine",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_place",
+          "type": "string"
+        }
+      ],
+      "name": "addRMS",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_place",
+          "type": "string"
+        }
+      ],
+      "name": "addRetailer",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "disCtr",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "manCtr",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "medicineCtr",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "retCtr",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "rmsCtr",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "showStage",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_medicineID",
+          "type": "uint256"
+        }
+      ],
+      "name": "sold",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
