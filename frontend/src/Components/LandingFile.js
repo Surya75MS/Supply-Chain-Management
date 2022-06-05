cef15a17869e7f8ee537db24b93f2fd447c98e20
Surya_M_S_7 <sur.ms2001@gmail.com>
2022-06-05 11:53:52 +0530

    Casual commit.

--- frontend/src/Components/LandingFile.js
+++ frontend/src/Components/LandingFile.js
@@ -1,45 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import Web3 from 'web3/dist/web3.min.js'
 
 const Landing_Comp = () => {
     const owner = '0xa39231A630EFd4E34F760cC499aA0BC915B43B75';
     const nav = useNavigate();
+    const [currentAccount, setcurrentAccount] = useState();
     const [State, setState] = useState("");
 
-    const CheckIfOwner = async () => {
-        // const web3 = await window.web3;
-        const web3 = new Web3(window.ethereum);
+    // const CheckIfOwner = async () => {
+    //     // const web3 = await window.web3;
+    //     const web3 = new Web3(window.ethereum);
 
-        const acc = await web3.eth.getAccounts().then(console.log);
-        setState({ account: acc[0] });
-        console.log(State);
-        if (acc == owner) {
+    //     const acc = await web3.eth.getAccounts().then(console.log);
+    //     setState({ account: acc[0] });
+    //     console.log(State);
+    //     if (acc == owner) {
+    //         console.log("True");
+    //     }
+    //     else {
+    //         console.log("False");
+    //     }
+    // };
+    // const Connect = async () => {
+    //     if (window.ethereum) {
+    //         window.web3 = new Web3(window.ethereum);
+    //         await window.ethereum.enable();
+    //         console.log("Im in if");
+    //         CheckIfOwner();
+
+    //     } else if (window.web3) {
+    //         window.web3 = new Web3(window.web3.currentProvider);
+    //         console.log("Im if else if");
+    //     } else {
+    //         window.alert(
+    //             "Non-Ethereum browser detected. You should consider trying MetaMask!"
+    //         );
+    //     }
+    // };
+    const Connect = async () => {
+        const account = await window.ethereum.request({ method: 'eth_requestAccounts' }).catch((error) => {
+            window.alert(error.message);
+            return;
+        });
+        window.alert(`Successfully Connected with ${account[0]}`);
+        setcurrentAccount(account[0]);
+    };
+
+    function CheckMetamaskInstalled() {
+        if (!window.ethereum) {
             console.log("True");
+            document.getElementById("ConnectButton").innerText = "Install Metamask";
         }
         else {
+            document.getElementById("ConnectButton").addEventListener('click', Connect);
             console.log("False");
         }
-    };
-    const Connect = async () => {
-        if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum);
-            await window.ethereum.enable();
-            console.log("Im in if");
-            CheckIfOwner();
+    }
 
-        } else if (window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider);
-            console.log("Im if else if");
-        } else {
-            window.alert(
-                "Non-Ethereum browser detected. You should consider trying MetaMask!"
-            );
-        }
-    };
+    useEffect(() => {
+
+        CheckMetamaskInstalled();
+    }, []);
 
     return (
         <div>
+            <div>Current Account: {currentAccount}</div>
             <header id="header" className="header fixed-top">
                 <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
 
@@ -47,11 +73,11 @@ const Landing_Comp = () => {
                         <img src="assets/img/logo.png" alt="" />
                         <span>DandI SCM</span>
                     </a>
-                    <li><a className="getstarted scrollto" onClick={CheckIfOwner}>Owner!</a></li>
+                    {/* <li><a className="getstarted scrollto" onClick={CheckIfOwner}>Owner!</a></li> */}
 
                     <nav id="navbar" className="navbar">
                         <ul>
-                            <li><a className="getstarted scrollto" onClick={Connect}>Connect Wallet!</a></li>
+                            <li><a id="ConnectButton" className="getstarted scrollto">Connect Wallet!</a></li>
                         </ul>
                         <i className="bi-list mobile-nav-toggle"></i>
                     </nav>
